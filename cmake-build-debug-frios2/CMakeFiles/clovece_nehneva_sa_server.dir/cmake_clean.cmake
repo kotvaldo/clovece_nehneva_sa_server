65d016d18b97baefe3e8bbd46cd0f5aa4d969502
adams <adamstano5@gmail.com>
2024-01-05 17:02:28 +0100

    initial

---
+++ cmake-build-debug-frios2/CMakeFiles/clovece_nehneva_sa_server.dir/cmake_clean.cmake
@@ -0,0 +1,10 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/clovece_nehneva_sa_server.dir/server.cpp.o"
+  "clovece_nehneva_sa_server"
+  "clovece_nehneva_sa_server.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/clovece_nehneva_sa_server.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
---
+++ cmake-build-debug-frios2/CMakeFiles/clovece_nehneva_sa_server.dir/depend.make
@@ -0,0 +1,2 @@
+# Empty dependencies file for clovece_nehneva_sa_server.
+# This may be replaced when dependencies are built.
